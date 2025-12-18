fbbb8b6562b8b6d874c85a9685407b40b286b15d
Dan <contact@danmyers.net>
2025-12-18 16:30:24 -0600

    updated

--- flake.nix
+++ flake.nix
@@ -8,11 +8,11 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      version = "1.16.1b";
+      version = "1.17.14b";
       downloadUrl = {
         "zen" = {
 	  url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
-	  sha256 = "sha256:04bb9f09kb0bmbgas5yb2qxnjh4gclq3bxnrzj1lijmf3w4240gd";
+	  sha256 = "sha256:1xzra71c1r32ggfj9xav9svvxxag0idq81gjx7q2xi08wi57qsp5";
 	};
       };
 
